bda3771befa35c62979e2f8d1c415a35ef36f24f
Ondrej Valas <ondrej.valas@student.tuke.sk>
2026-03-29 12:05:02 +0200

    pridanie funkice na decrease zivotov a vypis hangmana

--- hangman.py
+++ hangman.py
@@ -1,5 +1,5 @@
 import random
-from hangman_art import logo
+from hangman_art import logo,stages
 from hangman_words import word_list
 
 def print_logo():
@@ -9,9 +9,9 @@ def choose_word():
     ch = random.choice(word_list)
     return ch
 
-def parse_secret(ch):
+def parse_secret(secret):
     p = ""
-    for i in range(len(ch)):
+    for i in range(len(secret)):
         p += "_"
     return p
 
@@ -21,14 +21,30 @@ def player_guess():
         print("Enter valid character")
     return p_in
 
+def check_guess(guess, secret):
+    for i in secret:
+        if i == guess:
+            print("Your guess was right.")
+        else:
+            print("Your guess was wrong")
+
+def decrease_lives(live):
+    live -= 1
+
+def print_hangman(lives):
+    print(stages(lives))
+
 def game_loop():
     ch = choose_word()
-    pars = parse_secret(ch)
+    p = parse_secret(ch)
+    correct_letter = []
+    print(ch)
     lives = 6
     game_over = False
     print_logo()
     while not game_over:
-        player_guess()
+        check_guess(player_guess(),ch)
+
 
 
 
