696e7da1f3db3c0fa0eb7579f04d1f30013414d7
Ondrej Valas <ondrej.valas@student.tuke.sk>
2026-03-29 15:09:04 +0200

    funkcny hangman done

--- hangman.py
+++ hangman.py
@@ -21,31 +21,51 @@ def player_guess():
         print("Enter valid character")
     return p_in
 
-def check_guess(guess, secret):
-    for i in secret:
-        if i == guess:
-            print("Your guess was right.")
-        else:
-            print("Your guess was wrong")
-
-def decrease_lives(live):
-    live -= 1
 
 def print_hangman(lives):
     print(stages(lives))
 
+def check(guess, secret, already_guessed,lives):
+    if guess in secret:
+        already_guessed.append(guess)
+    else:
+        lives -= 1
+
+    word = ""
+    for i in secret:
+        if i in already_guessed:
+            word += i
+        else:
+            word += "_"
+
+    return word
+
+def win(dis,secret):
+    if dis == secret:
+        return True
+    return False
+
+
 def game_loop():
     ch = choose_word()
-    p = parse_secret(ch)
-    correct_letter = []
     print(ch)
+    displayed = parse_secret(ch)
+    print(displayed)
     lives = 6
+    alg = [] #pozor kde davas premeene vo while slucke sa resetuje
     game_over = False
     print_logo()
+
     while not game_over:
-        check_guess(player_guess(),ch)
+        g = player_guess()
+        displayed = check(g, ch, alg)
+        print(displayed)
 
+        if lives == 0:
+            print("Prehral si!")
+            game_over = True
 
-
-
+        if win(displayed,ch):
+            print("Vyhral si!")
+            game_over = True
 
